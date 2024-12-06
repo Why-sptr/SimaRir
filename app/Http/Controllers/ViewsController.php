ade6247d5e523c404de3231f0a06754a021fcdff
Wahyu Saputra <wahyucahyos380@gmail.com>
2024-12-06 22:27:14 +0700

    feat: route basic frontend

--- app/Http/Controllers/ViewsController.php
+++ app/Http/Controllers/ViewsController.php
@@ -9,4 +9,16 @@ class ViewsController extends Controller
     public function admin(){
         return view('admin.index');
     }
+    public function company(){
+        return view('company.index');
+    }
+    public function userCompany(){
+        return view('user.company');
+    }
+    public function userDetailCompany(){
+        return view('user.detail-company');
+    }
+    public function userLoker(){
+        return view('user.loker');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,7 @@ Route::get('/', function () {
 });
 
 Route::get('/admin',[ViewsController::class,'admin']);
+Route::get('/company',[ViewsController::class,'company']);
+Route::get('/user/company',[ViewsController::class,'userCompany']);
+Route::get('/user/detail-company',[ViewsController::class,'userDetailCompany']);
+Route::get('/user/loker',[ViewsController::class,'userLoker']);
