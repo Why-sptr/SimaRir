86092ae85e7a8ea44b7f2b2c2e254c4cd80d3657
Wahyu Saputra <wahyucahyos380@gmail.com>
2024-12-13 21:39:27 +0700

    feat: setup admin route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,18 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\CompanyController;
+use App\Http\Controllers\Admin\CorporateFieldController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\EducationController;
+use App\Http\Controllers\Admin\JobAdminController;
+use App\Http\Controllers\Admin\JobRoleController;
+use App\Http\Controllers\Admin\JobWorkController;
+use App\Http\Controllers\Admin\SkillController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\VerificationController;
+use App\Http\Controllers\Admin\WorkMethodController;
+use App\Http\Controllers\Admin\WorkTypeController;
 use App\Http\Controllers\ViewsController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -8,25 +21,16 @@ Route::get('/', function () {
     return view('user.loker');
 });
 
-Route::get('admin',[ViewsController::class,'admin'])->name('admin.dashboard');
-Route::get('admin/list-company',[ViewsController::class,'listCompany']);
-Route::get('admin/verification-company',[ViewsController::class,'verificationCompany']);
-Route::get('admin/job-admin',[ViewsController::class,'jobAdmin']);
-Route::get('admin/job-company',[ViewsController::class,'jobCompany']);
-Route::get('admin/list-user',[ViewsController::class,'listUser']);
-Route::get('admin/list-admin',[ViewsController::class,'listAdmin']);
-Route::get('admin/skill',[ViewsController::class,'skill']);
-Route::get('admin/company-field',[ViewsController::class,'companyField']);
-Route::get('/company',[ViewsController::class,'company'])->name('company.dashboard');
-Route::get('/company/detail-job',[ViewsController::class,'detailJob']);
-Route::get('/company/detail-user',[ViewsController::class,'detailUser']);
-Route::get('/user/company',[ViewsController::class,'userCompany']);
-Route::get('/user/detail-company',[ViewsController::class,'userDetailCompany']);
-Route::get('/user/loker',[ViewsController::class,'userLoker'])->name('user.loker');
-Route::get('/user/detail-loker',[ViewsController::class,'detailLoker']);
-Route::get('/user/lamaran-saya',[ViewsController::class,'lamaranSaya']);
-Route::get('/user/disimpan',[ViewsController::class,'disimpan']);
-Route::get('/user/profile',[ViewsController::class,'profile']);
+Route::get('/company', [ViewsController::class, 'company'])->name('company.dashboard');
+Route::get('/company/detail-job', [ViewsController::class, 'detailJob']);
+Route::get('/company/detail-user', [ViewsController::class, 'detailUser']);
+Route::get('/user/company', [ViewsController::class, 'userCompany']);
+Route::get('/user/detail-company', [ViewsController::class, 'userDetailCompany']);
+Route::get('/user/loker', [ViewsController::class, 'userLoker'])->name('user.loker');
+Route::get('/user/detail-loker', [ViewsController::class, 'detailLoker']);
+Route::get('/user/lamaran-saya', [ViewsController::class, 'lamaranSaya']);
+Route::get('/user/disimpan', [ViewsController::class, 'disimpan']);
+Route::get('/user/profile', [ViewsController::class, 'profile']);
 
 Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('login', [AuthController::class, 'login']);
@@ -34,3 +38,20 @@ Route::get('register', [AuthController::class, 'showRegisterForm'])->name('regis
 Route::post('register', [AuthController::class, 'register']);
 Route::get('login/google', [AuthController::class, 'redirectToGoogle']);
 Route::get('login/google/callback', [AuthController::class, 'handleGoogleCallback']);
+
+// Admin
+Route::prefix('admin')->group(function () {
+    Route::get('/', [DashboardController::class,'index']);
+    Route::resource('company', CompanyController::class);
+    Route::resource('verification', VerificationController::class);
+    Route::resource('corporate-field', CorporateFieldController::class);
+    Route::resource('education', EducationController::class);
+    Route::resource('job-role', JobRoleController::class);
+    Route::resource('job-work', JobWorkController::class);
+    Route::resource('job-admin', JobAdminController::class);
+    Route::resource('skill', SkillController::class);
+    Route::resource('user', UserController::class);
+    Route::resource('admin', AdminController::class);
+    Route::resource('work-method', WorkMethodController::class);
+    Route::resource('work-type', WorkTypeController::class);
+});
