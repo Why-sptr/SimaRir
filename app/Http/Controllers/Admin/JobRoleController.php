02f142fe16abc36f3f2957c836c28e4714b809d9
Wahyu Saputra <wahyucahyos380@gmail.com>
2024-12-14 17:08:22 +0700

    feat: CRUD job role

--- app/Http/Controllers/Admin/JobRoleController.php
+++ app/Http/Controllers/Admin/JobRoleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\JobRole;
 use Illuminate\Http\Request;
 
 class JobRoleController extends Controller
@@ -10,5 +11,41 @@ class JobRoleController extends Controller
     public function index()
     {
         return view('admin.job-role.index', ['title' => 'Role Pekerjaan']);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        JobRole::create($request->all());
+
+        return redirect()->route('job-role.index')
+            ->with('success', 'Role Pekerjaan berhasil ditambah.');
+    }
+
+    public function edit(JobRole $jobRole)
+    {
+        return view('admin.job-role.index', compact('jobRole'));
+    }
+
+    public function update(Request $request, JobRole $jobRole)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $jobRole->update($request->all());
+
+        return redirect()->route('job-role.index')->with('success', 'Role Pekerjaan berhasil diperbarui.');
+    }
+
+    public function destroy(JobRole $jobRole)
+    {
+        $jobRole->delete();
+
+        return redirect()->route('job-role.index')
+            ->with('success', 'Role Pekerjaan berhasil dihapus.');
     }
 }
