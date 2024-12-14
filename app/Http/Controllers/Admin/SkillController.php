f8bc4f2eb546fa7be588355ac0b56cbf585607fb
Wahyu Saputra <wahyucahyos380@gmail.com>
2024-12-14 17:08:31 +0700

    feat: CRUD skill

--- app/Http/Controllers/Admin/SkillController.php
+++ app/Http/Controllers/Admin/SkillController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Skill;
 use Illuminate\Http\Request;
 
 class SkillController extends Controller
@@ -10,5 +11,41 @@ class SkillController extends Controller
     public function index()
     {
         return view('admin.skill.index', ['title' => 'Skill']);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        Skill::create($request->all());
+
+        return redirect()->route('skill.index')
+            ->with('success', 'Skill berhasil ditambah.');
+    }
+
+    public function edit(Skill $skill)
+    {
+        return view('admin.skill.index', compact('skill'));
+    }
+
+    public function update(Request $request, Skill $skill)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $skill->update($request->all());
+
+        return redirect()->route('skill.index')->with('success', 'Skill berhasil diperbarui.');
+    }
+
+    public function destroy(Skill $skill)
+    {
+        $skill->delete();
+
+        return redirect()->route('skill.index')
+            ->with('success', 'Skill berhasil dihapus.');
     }
 }
