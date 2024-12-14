f966133addb5d675497d9419c7cc81c2f603ccbd
Wahyu Saputra <wahyucahyos380@gmail.com>
2024-12-14 17:08:41 +0700

    feat: CRUD work method

--- app/Http/Controllers/Admin/WorkMethodController.php
+++ app/Http/Controllers/Admin/WorkMethodController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\WorkMethod;
 use Illuminate\Http\Request;
 
 class WorkMethodController extends Controller
@@ -10,5 +11,41 @@ class WorkMethodController extends Controller
     public function index()
     {
         return view('admin.work-method.index', ['title' => 'Metode Pekerjaan']);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        WorkMethod::create($request->all());
+
+        return redirect()->route('work-method.index')
+            ->with('success', 'Metode Pekerjaan berhasil ditambah.');
+    }
+
+    public function edit(WorkMethod $workMethod)
+    {
+        return view('admin.work-method.index', compact('workMethod'));
+    }
+
+    public function update(Request $request, WorkMethod $workMethod)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $workMethod->update($request->all());
+
+        return redirect()->route('work-method.index')->with('success', 'Metode Pekerjaan berhasil diperbarui.');
+    }
+
+    public function destroy(WorkMethod $workMethod)
+    {
+        $workMethod->delete();
+
+        return redirect()->route('work-method.index')
+            ->with('success', 'Metode Pekerjaan berhasil dihapus.');
     }
 }
