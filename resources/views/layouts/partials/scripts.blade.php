2bb7fd88ce9a848b93777918400c493458b21246
Wahyu Saputra <wahyucahyos380@gmail.com>
2024-12-19 16:09:51 +0700

    fix: return back asset style

--- resources/views/layouts/partials/scripts.blade.php
+++ resources/views/layouts/partials/scripts.blade.php
@@ -1,14 +1,13 @@
 <!-- resources/views/partials/scripts.blade.php -->
 <!-- Vendor JS Files -->
-<script src="{{ mix('assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
-<script src="{{ mix('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ mix('assets/vendor/chart.js/chart.umd.js') }}"></script>
-<script src="{{ mix('assets/vendor/echarts/echarts.min.js') }}"></script>
-<script src="{{ mix('assets/vendor/quill/quill.js') }}"></script>
-<script src="{{ mix('assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
-<script src="{{ mix('assets/vendor/tinymce/tinymce.min.js') }}"></script>
-<script src="{{ mix('assets/vendor/php-email-form/validate.js') }}"></script>
+<script src="{{ asset('assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/chart.js/chart.umd.js') }}"></script>
+<script src="{{ asset('assets/vendor/echarts/echarts.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/quill/quill.js') }}"></script>
+<script src="{{ asset('assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+<script src="{{ asset('assets/vendor/tinymce/tinymce.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
 
 <!-- Template Main JS File -->
-<script src="{{ mix('assets/js/main.js') }}"></script>
-
+<script src="{{ asset('assets/js/main.js') }}"></script>
