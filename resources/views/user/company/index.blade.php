d03ce79de66ae79c66b3e80c034422ba61c4c926
Wahyu Saputra <wahyucahyos380@gmail.com>
2025-02-20 20:58:05 +0700

    feat: styling user company

--- resources/views/user/company/index.blade.php
+++ resources/views/user/company/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Perusahaan</title>
-    <link rel="stylesheet" href="/style.css" />
+    <link rel="stylesheet" href="../css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
@@ -17,44 +17,79 @@
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
         crossorigin="anonymous"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet" />
+    <script src="https://unpkg.com/@phosphor-icons/web@2.1.1"></script>
 </head>
 
 <body>
-    <section>
-        <div class="container py-5">
-            <div class="d-flex justify-content-center ">
-                <div class="input-group mb-3 w-75">
-                    <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="button-addon2">
-                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Cari</button>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg">
+        <div class="container p-3">
+            <a class="navbar-brand" href="#" class="action"><i class="ph-duotone ph-book"></i></a>
+            <div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav gap-3">
+                        <li class="nav-item">
+                            <a class="nav-link fw-semibold" href="/user-profile">Profile</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link fw-semibold" href="/user-job-work">Pekerjaan</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active text-primary fw-bold" href="/user-company">Perusahaan</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link fw-semibold" href="/apply">Lamaran</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link fw-semibold" href="/favorite">Disimpan</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
-            <div class="row g-4">
+        </div>
+    </nav>
+    <section>
+        <div class="container py-5">
+            <div class="container mb-4">
+                <div class="row justify-content-center">
+                    <div class="col-md-6">
+                        <div class="search-container">
+                            <input type="text" class="form-control search-input" placeholder="Search...">
+                            <i class="ph-duotone ph-magnifying-glass search-icon"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row g-3">
                 @foreach ($companies as $company)
                 <!-- Card 1 -->
                 <div class="col-md-4">
                     <a href="{{ route('user-company.show', $company->id) }}" style="text-decoration: none; color: inherit;">
-                        <div class="card h-100">
+                        <div class="card h-100 border border-1 border-primary">
                             <div class="card-body">
                                 <div class="d-flex align-items-center mb-2">
-                                    <img src="{{ asset('storage/avatars/' . $company->user->avatar) }}" alt="Company Image" style="max-width: 50px; max-height: 50px; width:100%; height:100%; object-fit: cover;" class="img-fluid rounded-2 shadow-sm me-2">
+                                    <img src="{{ asset('storage/avatars/' . $company->user->avatar) }}" alt="Company Image" style="max-width: 50px; max-height: 50px; width:100%; height:100%; object-fit: cover;" class="img-fluid rounded-2 border border-primary me-2">
                                     <div>
-                                        <h5 class="card-title">{{ $company->user->name }}</h5>
+                                        <h5 class="card-title text-dark">{{ $company->user->name }}</h5>
                                         <div class="d-flex gap-2 align-items-center">
-                                            <i class="fa-solid fa-location-dot"></i>
-                                            <p class="card-text text-muted">{{ $company->user->location }}</p>
+                                            <i class="ph-duotone ph-map-pin"></i>
+                                            <small class="card-text text-muted">{{ $company->user->location }}</small>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="d-flex gap-2 align-items-center">
-                                    <i class="fa-solid fa-building text-center" style="width: 24px;"></i>
+                                    <i class="ph-duotone ph-buildings" style="width: 24px;"></i>
                                     <p class="card-text"><strong>{{ $company->corporateField->name }}</strong></p>
                                 </div>
                                 <div class="d-flex gap-2 align-items-center">
-                                    <i class="fa-solid fa-briefcase text-center" style="width: 24px;"></i>
+                                    <i class="ph-duotone ph-read-cv-logo" style="width: 24px;"></i>
                                     <p class="card-text">{{ $company->jobWorks->count() }} lowongan</p>
                                 </div>
                                 <hr>
-                                <p class="card-text text-muted">{{ $company->employee }} Karyawan</p>
+                                <small class="card-text text-muted">{{ $company->employee }} Karyawan</small>
                             </div>
                         </div>
                     </a>
