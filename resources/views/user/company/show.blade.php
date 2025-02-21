6d56e22230e692f960befe72b8b48ab3f9019dd1
Wahyu Saputra <wahyucahyos380@gmail.com>
2025-02-21 09:44:41 +0700

    feat: styling user detai company

--- resources/views/user/company/show.blade.php
+++ resources/views/user/company/show.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Detail Perusahaan</title>
-    <link rel="stylesheet" href="/style.css" />
+    <link rel="stylesheet" href="../css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
@@ -17,17 +17,48 @@
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
         crossorigin="anonymous"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet" />
+    <script src="https://unpkg.com/@phosphor-icons/web@2.1.1"></script>
 </head>
 
 <body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg">
+        <div class="container p-3">
+            <a class="navbar-brand" href="#" class="action"><i class="ph-duotone ph-book"></i></a>
+            <div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav gap-3">
+                        <li class="nav-item">
+                            <a class="nav-link fw-semibold" href="/user-profile">Profile</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link fw-semibold" href="/user-job-work">Pekerjaan</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link fw-semibold" href="/user-company">Perusahaan</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link fw-semibold" href="/apply">Lamaran</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link fw-semibold" href="/favorite">Disimpan</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
     <section>
         <div class="container mt-4">
-            <div class="card p-3 shadow-sm border-0">
+            <div class="card p-3 border-1 border-primary">
                 <div class="row align-items-center">
                     <!-- Image Section -->
                     <div class="col-md-4">
-                        <div class="d-flex justify-content-center align-items-center bg-light" style="height: 200px; border: 1px solid #ddd;">
-                            <img src="{{ asset('storage/avatars/' . $company->user->avatar) }}" alt="Profile Avatar" style="max-width: 500px; max-height: 200px; width:100%; height:100%; object-fit: cover;" class="img-fluid rounded-2 shadow-sm">
+                        <div class="d-flex justify-content-center align-items-center bg-light" style="height: 200px;">
+                            <img src="{{ asset('storage/avatars/' . $company->user->avatar) }}" alt="Profile Avatar" style="max-width: 500px; max-height: 200px; width:100%; height:100%; object-fit: cover;  border: 1px solid #ddd;" class="img-fluid rounded-2">
                         </div>
                     </div>
                     <!-- Text and Details Section -->
@@ -37,25 +68,25 @@
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="d-flex gap-2 align-items-center">
-                                    <i class="fa-solid fa-location-dot" style="width: 24px;"></i>
-                                    <p class="card-text fw-semibold">{{ $company->user->location }}</p>
+                                    <i class="ph-duotone ph-map-pin text-primary" style="width: 24px;"></i>
+                                    <p class="card-text">{{ $company->user->location }}</p>
                                 </div>
                                 <div class="d-flex gap-2 align-items-center">
-                                    <i class="fa-solid fa-building" style="width: 24px;"></i>
+                                    <i class="ph-duotone ph-buildings text-primary" style="width: 24px;"></i>
                                     <p class="card-text">{{ $company->corporateField->name }}</p>
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="d-flex gap-2 align-items-center">
-                                    <i class="fa-solid fa-users" style="width: 24px;"></i>
+                                    <i class="ph-duotone ph-users-three text-primary" style="width: 24px;"></i>
                                     <p class="card-text">{{ $company->employee }}</p>
                                 </div>
                                 <div class="d-flex gap-2 align-items-center">
-                                    <i class="fa-solid fa-check" style="width: 24px;"></i>
+                                    <i class="ph-duotone ph-seal-check" style="width: 24px; color: blue;"></i>
                                     @if ($company->status_verification == 0)
-                                    <span class="badge bg-warning p-2">Belum Terverifikasi</span>
+                                    <span class="badge bg-secondary p-2">Belum Terverifikasi</span>
                                     @elseif ($company->status_verification == 1)
-                                    <span class="badge bg-success p-2">Terverifikasi</span>
+                                    <span class="badge bg-success p-2" style="background-color: blue !important;">Terverifikasi</span>
                                     @endif
                                 </div>
                             </div>
@@ -66,15 +97,15 @@
         </div>
 
         <div class="container mt-4">
-            <div class="card p-3 shadow-sm border-0">
+            <div class="card p-3 border-1 border-primary">
                 <h5 class="fw-bold">Lowongan Pekerjaan</h5>
                 <div class="row g-4" id="job-list">
                     @foreach ($jobWorks as $jobWork)
                     <div class="col-md-6 mb-4 d-flex">
-                        <div class="card shadow-sm border-0 w-100 h-100">
+                        <div class="card border-1 w-100 h-100">
                             <a class="card-body d-flex flex-column" href="{{ route('user-job-work.show', $jobWork->id) }}" style="text-decoration: none; color: inherit;">
-                                <div class="d-flex justify-content-between gap-2">
-                                    <h5 class="card-title text-truncate" style="width: 85%;">
+                                <div class="d-flex justify-content-between gap-2 px-3 mt-3">
+                                    <h5 class="card-title text-truncate fw-semibold" style="width: 85%;">
                                         {{ $jobWork->name }}
                                     </h5>
                                     <p class="text-primary fw-semibold text-end">
@@ -91,19 +122,19 @@
                                         {{ $salary }}
                                     </p>
                                 </div>
-                                <div class="d-flex flex-column flex-grow-1">
+                                <div class="d-flex flex-column flex-grow-1 px-3 mb-3">
                                     <div class="d-flex flex-wrap mb-3 gap-1">
-                                        <span class="badge bg-secondary p-2">{{ $jobWork->workMethod->name }}</span>
-                                        <span class="badge bg-secondary p-2">{{ $jobWork->workType->name }}</span>
-                                        <span class="badge bg-secondary p-2">{{ $jobWork->qualification->work_experience }} Tahun</span>
-                                        <span class="badge bg-secondary p-2">{{ $jobWork->qualification->education->name }}</span>
+                                        <span class="badge badge-outline-primary p-2">{{ $jobWork->workMethod->name }}</span>
+                                        <span class="badge badge-outline-primary p-2">{{ $jobWork->workType->name }}</span>
+                                        <span class="badge badge-outline-primary p-2">{{ $jobWork->qualification->work_experience }} Tahun</span>
+                                        <span class="badge badge-outline-primary p-2">{{ $jobWork->qualification->education->name }}</span>
                                         @if ($jobWork->qualification->major)
-                                        <span class="badge bg-secondary p-2">{{ $jobWork->qualification->major }}</span>
+                                        <span class="badge badge-outline-primary p-2">{{ $jobWork->qualification->major }}</span>
                                         @endif
                                         @if ($jobWork->qualification->ipk)
-                                        <span class="badge bg-secondary p-2">IPK {{ $jobWork->qualification->ipk }}</span>
+                                        <span class="badge badge-outline-primary p-2">IPK {{ $jobWork->qualification->ipk }}</span>
                                         @endif
-                                        <span class="badge bg-secondary p-2">+ {{ $jobWork->skillJobs->count() + 1 }}</span>
+                                        <span class="badge badge-outline-primary p-2">+ {{ $jobWork->skillJobs->count() + 1 }}</span>
                                     </div>
                                     <div class="mt-auto">
                                         <div>
@@ -133,7 +164,7 @@
         </div>
 
         <div class="container mt-4">
-            <div class="card shadow-sm border-0 p-3">
+            <div class="card border-1 border-primary p-3">
                 <h5 class="fw-bold">Deskripsi Perusahaan</h5>
                 <div class="text">
                     {!! $company->user->description !!}
@@ -142,11 +173,11 @@
         </div>
 
         <div class="container my-4">
-            <div class="card shadow-sm border-0 p-4">
+            <div class="card border-1 border-primary p-3">
                 <h5 class="fw-bold">Informasi Perusahaan</h5>
                 <div class="row">
                     <div class="col-md-6">
-                        <div class="card shadow-sm border-0 h-100 p-3">
+                        <div class="card border-1 h-100 p-3">
                             <h6 class="mb-3">Hari dan Jam Kerja</h6>
                             <ul class="list-unstyled">
                                 <li class="row">
@@ -209,18 +240,17 @@
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <div class="card shadow-sm border-0 h-100 p-3">
+                        <div class="card border-1 h-100 p-3">
                             <h6 class="mb-3">Informasi Lainnya</h6>
                             <ul class="list-unstyled">
                                 {{-- Menampilkan Email Pengguna --}}
                                 <li>
                                     <p>
-                                        <span class="badge bg-light text-dark">
-                                            <i class="fa-solid fa-envelope"></i>
+                                        <span class="badge bg-primary rounded-pill p-2">
+                                            <i class="ph-duotone ph-envelope"></i>
                                         </span> {{ $company->user->email }}
                                     </p>
                                 </li>
-
                                 {{-- Periksa Media Sosial --}}
                                 @if ($company->user && $company->user->socialMedia)
                                 @php
@@ -239,11 +269,11 @@
                                 @if ($link)
                                 <li>
                                     <p>
-                                        <span class="badge bg-light text-dark">
+                                        <span class="badge bg-primary rounded-pill p-2">
                                             @if ($platform === 'website')
-                                            <i class="fa-solid fa-globe"></i> {{-- Ikon untuk Website --}}
+                                            <i class="ph-duotone ph-globe"></i> {{-- Ikon untuk Website --}}
                                             @else
-                                            <i class="fa-brands fa-{{ $platform }}"></i> {{-- Ikon untuk Platform Lain --}}
+                                            <i class="ph-duotone ph-{{ $platform }}-logo"></i> {{-- Ikon untuk Platform Lain --}}
                                             @endif
                                         </span>
                                         <a href="{{ $link }}" target="_blank">
@@ -254,7 +284,9 @@
                                 @endif
                                 @endforeach
                                 @else
-                                <p>Media sosial belum ditambahkan.</p>
+                                <div class="text-center">
+                                    <img src="{{ asset('assets/img/notfound.png') }}" class="opacity-50 w-100" alt="">
+                                </div>
                                 @endif
                             </ul>
                         </div>
@@ -264,7 +296,7 @@
         </div>
 
         <div class="container mt-4">
-            <div class="card shadow-sm h-100 border-0 p-3">
+            <div class="card h-100 border-1 border-primary p-3">
                 <h5 class="fw-bold mb-3">Galeri Perusahaan</h5>
                 <div class="row g-4">
                     @foreach ($company->galleries as $gallery)
@@ -278,7 +310,7 @@
                         <div class="col-4">
                             <img src="{{ $imagePath }}"
                                 alt="Company Galeri {{ $i }}"
-                                style="max-height: 500px; max-width: 500px; width: 100%; height: 100%; object-fit: cover;"
+                                style="max-height: 500px; max-width: 500px; width: 100%; height: 100%; object-fit: cover; border: 1px solid #ddd;"
                                 class="rounded w-100">
                         </div>
                         @endif
