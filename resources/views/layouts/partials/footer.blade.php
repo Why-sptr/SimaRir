5262f83a9e44f280c3e1eb599fb66bae76adb127
Wahyu Saputra <wahyucahyos380@gmail.com>
2025-05-17 23:46:57 +0700

    fix: fix footer text admin

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer id="footer" class="footer">
     <div class="copyright">
-      &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
+      &copy; Copyright <strong><span>Simarir</span></strong>. All Rights Reserved
     </div>
   </footer><!-- End Footer -->
   
